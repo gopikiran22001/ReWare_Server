c3b7f88627719de837fd63e58d9bd7c0f3387089
gopikiran <gopikiran22001@yahoo.com>
2025-07-27 13:51:25 +0530

    27-07-2025

--- ml_server.py
+++ ml_server.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, jsonify
 import joblib
 import pandas as pd
+from waitress import serve
+import logging
+
 
 # Load model
 model = joblib.load('model_pipeline.pkl')
@@ -9,14 +12,20 @@ app = Flask(__name__)
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.get_json()
-    df = pd.DataFrame([data])  # Convert input to DataFrame
-    prediction = model.predict(df)[0]
-    return jsonify({
-        'co2_emissions': round(prediction[0], 2),
-        'water_consumption': round(prediction[1], 2)
-    })
+    try:
+        data = request.get_json()
+        df = pd.DataFrame([data])
+        prediction = model.predict(df)[0]
+        return jsonify({
+            'co2_emissions': round(prediction[0], 2),
+            'water_consumption': round(prediction[1], 2)
+        })
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
-    # print("Server is running http://localhost:5000")
+    logging.basicConfig(level=logging.INFO)
+    logging.info("🚀 Starting server at http://0.0.0.0:5000")
+    serve(app, host='0.0.0.0', port=5000)
+
